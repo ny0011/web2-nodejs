fd35927bd62bee46cc3d316da0999d2a3cfe54e6
ny0011 <ny0011@github.com>
2019-05-07 17:46:31 +0900

    [190507] Node.js - URL로 입력된 값 사용하기

--- README.md
+++ README.md
@@ -17,6 +17,22 @@ localhost:3000 (3000은 listen값)에서 웹 페이지를 볼 수 있음
 줄바꿈을 하고 싶을 때 ``로 문자열을 묶자
 변수는 + var + 로 묶는게 아니라 ${var} 로 표기
 
+[Node.js - URL로 입력된 값 사용하기]
+- URL의 의미
+http://opentutorials.org:3000/main?id=HTML&page=12
+http : protocol
+opentutorials.org : host(domain)
+3000 : port (한 컴퓨터에 여러 서버가 있을 수 있으니 어떤 서버로 접속할 지 선택)
+main : path
+id=HTML&page=12 : query string(웹 서버에게 데이터를 전송할 수 있음)
+
+- Node.js에서 URL을 통해서 입력된 값을 사용하는 방법
+검색어 : nodejs url parse query string
+    var queryData = url.parse(request.url, true).query;
+    if (queryData.name) {
+        // user told us their name in the GET request, ex: http://host:8000/?name=Tom
+
+
 ```
 
 
--- main.js
+++ main.js
@@ -1,18 +1,25 @@
 var http = require('http');
 var fs = require('fs');
+var url = require('url'); // url은 모듈임. node.js에게 url 모듈을 사용한다고 알림
+
 var app = http.createServer(function(request,response){
-    var url = request.url;
-    if(request.url == '/'){
-      url = '/index.html';
+    var _url = request.url;
+    console.log(_url);
+
+    var queryData = url.parse(_url, true).query;
+    console.log(queryData);
+    console.log(queryData.id); // url의 query data로 ?id=HTML이라고 썼다면 HTML값이 출력됨
+
+    if(_url == '/'){
+      _url = '/index.html';
     }
-    if(request.url == '/favicon.ico'){
+    if(_url == '/favicon.ico'){
         response.writeHead(404);
         response.end();
         return;
     }
     response.writeHead(200);
-    console.log(__dirname + url);
-    response.end(fs.readFileSync(__dirname + url));
+    response.end(queryData.id); // 웹 페이지로 내용을 전송. queryData.id 값을 보냄
 
 });
 app.listen(3000);
