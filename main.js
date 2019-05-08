e01aa5f3d8f8525abf8e4dc91abd71682d2fbb8b
ny0011 <ny0011@github.com>
2019-05-08 17:06:10 +0900

    [190508] App - 글목록 출력하기

--- main.js
+++ main.js
@@ -2,17 +2,33 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url'); // url은 모듈임. node.js에게 url 모듈을 사용한다고 알림
 
-var app = http.createServer(function(request,response){
+var app = http.createServer(function(request, response) {
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
     console.log(url.parse(_url, true));
 
-    if(pathname === '/') {
-        if(queryData.id === undefined){
-            fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+    if (pathname === '/') {
+        if (queryData.id === undefined) {
+
+            fs.readdir('./data', function(error, filelist) {
+                console.log(filelist);
                 var title = 'Welcome';
                 var description = 'Hello, Node.js';
+
+                /*    var list = ` <ol>
+                       <li><a href="/?id=HTML">HTML</a></li>
+                       <li><a href="/?id=CSS">CSS</a></li>
+                       <li><a href="/?id=JavaScript">JavaScript</a></li>
+                     </ol>` */
+                var list = '<ul>';
+                var i = 0;
+                while (i < filelist.length) {
+                    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
+                    i = i + 1;
+                }
+                list = list + '</ul>';
+
                 var template = `
                 <!doctype html>
                 <html>
@@ -22,23 +38,32 @@ var app = http.createServer(function(request,response){
                 </head>
                 <body>
                   <h1><a href="/">WEB</a></h1>
-                  <ol>
-                    <li><a href="/?id=HTML">HTML</a></li>
-                    <li><a href="/?id=CSS">CSS</a></li>
-                    <li><a href="/?id=JavaScript">JavaScript</a></li>
-                  </ol>
+                  ${list}
                   <h2>${title}</h2>
                   <p>${description}</p>
                 </body>
                 </html>
                 `;
                 response.writeHead(200);
-                response.end(template);   //웹 페이지로 내용을 전송. queryData.id 값을 보냄
+                response.end(template); //웹 페이지로 내용을 전송. queryData.id 값을 보냄
             });
-        }else{
-            fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
-                var title = queryData.id;
-                var template = `
+
+        } else {
+            fs.readdir('./data', function(error, filelist) {
+                console.log(filelist);
+                var title = 'Welcome';
+                var description = 'Hello, Node.js';
+                var list = '<ul>';
+                var i = 0;
+                while (i < filelist.length) {
+                    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
+                    i = i + 1;
+                }
+                list = list + '</ul>';
+
+                fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description) {
+                    var title = queryData.id;
+                    var template = `
                 <!doctype html>
                 <html>
                 <head>
@@ -47,30 +72,21 @@ var app = http.createServer(function(request,response){
                 </head>
                 <body>
                   <h1><a href="/">WEB</a></h1>
-                  <ol>
-                    <li><a href="/?id=HTML">HTML</a></li>
-                    <li><a href="/?id=CSS">CSS</a></li>
-                    <li><a href="/?id=JavaScript">JavaScript</a></li>
-                  </ol>
+                  ${list}
                   <h2>${title}</h2>
                   <p>${description}</p>
                 </body>
                 </html>
                 `;
-                response.writeHead(200);
-                response.end(template);   //웹 페이지로 내용을 전송. queryData.id 값을 보냄
+                    response.writeHead(200);
+                    response.end(template); //웹 페이지로 내용을 전송. queryData.id 값을 보냄
+                });
             });
         }
-
-    }else{
+    } else {
         response.writeHead(404);
         response.end('Not Found');
     }
-
-
-
-
-
 
 });
 app.listen(3000);
