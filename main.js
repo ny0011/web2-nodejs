305d0043678dc190f97b7f9c2ece2ada260474f0
ny0011 <ny0011@github.com>
2019-05-23 17:46:16 +0900

    [190523] App - 글삭제 - 삭제버튼 구현
    삭제버튼을 링크로 만들면 안됨!
    form으로 만들고 꼭 post방식으로 메소드 보내자

--- main.js
+++ main.js
@@ -58,7 +58,13 @@ var app = http.createServer(function(request, response) {
                     var list = templateList(filelist);
                     var template = templateHTML(title, list,
                         `<h2>${title}</h2><p>${description}</p>`,
-                        `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+                        `<a href="/create">create</a>
+                        <a href="/update?id=${title}">update</a>
+                        <form action="delete_process" method="post">
+                        <input type="hidden" name="id" value="${title}">
+                        <input type="submit" value="delete">
+                        </form>
+                        `
                         );
                     response.writeHead(200);
                     response.end(template); //웹 페이지로 내용을 전송. queryData.id 값을 보냄
@@ -159,7 +165,6 @@ var app = http.createServer(function(request, response) {
                 })
             })
         });
-
     }
     else {
         response.writeHead(404);
